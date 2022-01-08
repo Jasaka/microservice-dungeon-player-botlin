6ddc96e0fa250b81bea147d11c84258b6685b034
andma <andreas.ma@smail.th-koeln.de>
2022-01-08 14:37:16 +0100

    chore:added exlusion for DataSourceAutoConfiguration since we have no data source configured yet

--- src/main/kotlin/thkoeln/dungeon/botlin/BotlinApplication.kt
+++ src/main/kotlin/thkoeln/dungeon/botlin/BotlinApplication.kt
@@ -1,9 +1,10 @@
 package thkoeln.dungeon.botlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude=[DataSourceAutoConfiguration::class])
 open class BotlinApplication
 
 fun main(args: Array<String>) {
