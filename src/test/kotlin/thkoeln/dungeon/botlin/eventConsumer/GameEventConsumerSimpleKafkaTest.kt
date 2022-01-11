b79826ee4e391c09d9389fa2886d8c22da17c4dc
andma <andreas.ma@smail.th-koeln.de>
2022-01-11 13:01:13 +0100

    feat: mock producer works docs: test fails because string parser doesnt work

--- src/test/kotlin/thkoeln/dungeon/botlin/eventConsumer/GameEventConsumerSimpleKafkaTest.kt
+++ src/test/kotlin/thkoeln/dungeon/botlin/eventConsumer/GameEventConsumerSimpleKafkaTest.kt
@@ -1,51 +1,73 @@
 package thkoeln.dungeon.botlin.eventConsumer;
 
-import org.apache.kafka.clients.producer.KafkaProducer
-import org.apache.kafka.clients.producer.MockProducer
-import org.apache.kafka.clients.producer.Producer
-import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.common.serialization.Serializer
-import org.apache.kafka.common.serialization.StringSerializer
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.test.EmbeddedKafkaBroker
 import org.springframework.kafka.test.context.EmbeddedKafka
+import org.springframework.kafka.test.utils.ContainerTestUtils
+import org.springframework.kafka.test.utils.KafkaTestUtils
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.junit4.SpringRunner
 import thkoeln.dungeon.botlin.eventConsumer.game.GameEventConsumerService
-import thkoeln.dungeon.botlin.eventConsumer.game.GameStatusEventPayload
-import thkoeln.dungeon.botlin.eventConsumer.game.GameStatusEventRepository
-import thkoeln.dungeon.botlin.eventConsumer.mock.MockGameEventProducer
-import thkoeln.dungeon.botlin.game.domain.GameStatus
-import java.util.*
+
 
 @RunWith(SpringRunner::class)
-@SpringBootTest
+@SpringBootTest(classes = [GameEventConsumerService::class])
 @DirtiesContext
-@EmbeddedKafka(topics = ["status"])
+@EnableKafka
+@EnableAutoConfiguration(exclude = [DataSourceAutoConfiguration::class])
+@EmbeddedKafka(topics = ["status"], partitions = 1, brokerProperties = ["listeners=PLAINTEXT://localhost:9092", "port=9092"])
 class GameEventConsumerSimpleKafkaTest {
+
     @Autowired
     lateinit var gameStatusEventConsumerService: GameEventConsumerService
 
-    lateinit var producer: MockProducer<String, String>
+    @Autowired //IntelliJ wrongfully says this is not found
+    lateinit var kafkaEmbedded: EmbeddedKafkaBroker
+
+    @Autowired
+    lateinit var kafkaListenerEndpointRegistry: KafkaListenerEndpointRegistry;
+
+    private lateinit var producer: KafkaTemplate<String, String>
 
     @Before
-    fun setUp() {
-        producer = MockProducer(true,
-                StringSerializer(),
-                StringSerializer())
+    fun buildProducer() {
+        this.producer = buildKafkaTemplate()
+    }
+
+    private fun buildKafkaTemplate(): KafkaTemplate<String, String> {
+        var senderProps = KafkaTestUtils.producerProps(kafkaEmbedded)
+        var pf: ProducerFactory<String, String> = DefaultKafkaProducerFactory(senderProps)
+        return KafkaTemplate(pf)
     }
 
 
+    @Before
+    fun setUp() {
+        for (messageListenerContainer in kafkaListenerEndpointRegistry.listenerContainers) {
+            ContainerTestUtils.waitForAssignment(messageListenerContainer, kafkaEmbedded.partitionsPerTopic)
+        }
+    }
+
     @Test
-    fun testConsume_PersistValidGameStatus_PersistedGameStatusEvent() {
-        producer.send(ProducerRecord("status", "GAME_FINISHED"))
-        assertTrue(gameStatusEventConsumerService.getGameStatusEventRepoSize()!! > 0)
+    fun testConsume_receiveMessage() {
+        //var kafkaTemplate = TestConfig().kafkaTemplate()
+        producer.send("status", "{\n" +
+                "  \"gameId\": \"5bc9f935-32f1-4d7b-a90c-ff0e6e34125a\",\n" +
+                "  \"status\": \"created\"\n" +
+                "}")
+        println("Game Event List ${GameEventConsumerService.gameEventList}")
+        assertThat(GameEventConsumerService.gameEventList.size != 0).isTrue
     }
 }
