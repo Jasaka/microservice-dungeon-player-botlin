d95beb55612c4b192bf33858ec62fa3ca95e2d4c
andma <andreas.ma@smail.th-koeln.de>
2022-01-13 15:41:35 +0100

    fix: TEST FINALLY PASSES!!!

--- src/test/kotlin/thkoeln/dungeon/botlin/eventConsumer/GameEventConsumerSimpleKafkaTest.kt
+++ src/test/kotlin/thkoeln/dungeon/botlin/eventConsumer/GameEventConsumerSimpleKafkaTest.kt
@@ -5,10 +5,13 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import kafka.utils.Json
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.header.internals.RecordHeader
+import org.apache.kafka.test.TestCondition
+import org.apache.kafka.test.TestUtils
 import org.assertj.core.api.Assertions.assertThat
 import org.json.JSONObject
 import org.junit.Before
 import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
@@ -30,18 +33,22 @@ import org.springframework.test.context.junit4.SpringRunner
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.JsonSerializer
 import thkoeln.dungeon.botlin.eventConsumer.game.GameEventConsumerService
 import thkoeln.dungeon.botlin.eventConsumer.game.GameStatusEvent
+import thkoeln.dungeon.botlin.game.application.GameApplicationService
+import thkoeln.dungeon.botlin.game.domain.GameRepository
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
 
 @RunWith(SpringRunner::class)
-@SpringBootTest(classes = [GameEventConsumerService::class])
-@DirtiesContext
-@EnableKafka
-@EnableAutoConfiguration(exclude = [DataSourceAutoConfiguration::class])
+@SpringBootTest
+@EnableAutoConfiguration
 @EmbeddedKafka(topics = ["status"], partitions = 1, brokerProperties = ["listeners=PLAINTEXT://localhost:9092", "port=9092"])
 class GameEventConsumerSimpleKafkaTest {
 
     @Autowired
+    lateinit var gameRepository: GameRepository
     lateinit var gameStatusEventConsumerService: GameEventConsumerService
+    lateinit var gameApplicationService: GameApplicationService
 
     @Autowired //IntelliJ wrongfully says this is not found
     lateinit var kafkaEmbedded: EmbeddedKafkaBroker
@@ -49,6 +56,7 @@ class GameEventConsumerSimpleKafkaTest {
     @Autowired
     lateinit var kafkaListenerEndpointRegistry: KafkaListenerEndpointRegistry;
 
+    private var latch = CountDownLatch(1);
     private lateinit var producer: KafkaTemplate<String, String>
 
     fun buildProducer() {
@@ -62,9 +70,11 @@ class GameEventConsumerSimpleKafkaTest {
         return KafkaTemplate(pf)
     }
 
-
     @Before
     fun setUp() {
+        gameRepository.deleteAll()
+        gameApplicationService = GameApplicationService(gameRepository)
+        gameStatusEventConsumerService = GameEventConsumerService(gameApplicationService)
         buildProducer()
         for (messageListenerContainer in kafkaListenerEndpointRegistry.listenerContainers) {
             ContainerTestUtils.waitForAssignment(messageListenerContainer, kafkaEmbedded.partitionsPerTopic)
@@ -75,7 +85,7 @@ class GameEventConsumerSimpleKafkaTest {
         var headerString = "{\"eventId\":\"5bc9f935-32f1-4d7b-a90c-ff0e6e34125a\"," +
                 "\"transactionId\":\"0cfc04f1-6df5-42c6-a19a-146128b8a3b4\"," +
                 "\"version\":42," +
-                "\"timestamp\":{}," +
+                "\"timestamp\":0," +
                 "\"type\":\"event-example-uploaded\"}"
 
         var payloadString =  "{\"gameId\":\"5bc9f935-32f1-4d7b-a90c-ff0e6e34125a\",\"status\":\"created\"}"
@@ -83,7 +93,7 @@ class GameEventConsumerSimpleKafkaTest {
         var producerRecord: ProducerRecord<String, String> = ProducerRecord<String, String>("status", payloadString)
         var jsontree: JsonNode = mapper.readTree(headerString)
         for (header in jsontree.fields()) {
-            producerRecord.headers().add(RecordHeader(header.key.toString(), header.value.toString().toByteArray()))
+            producerRecord.headers().add(RecordHeader(header.key.toString(), header.value.asText().toByteArray()))
         }
         return producerRecord
     }
@@ -93,6 +103,7 @@ class GameEventConsumerSimpleKafkaTest {
         var message = buildRecordHeader();
         producer.send(message)
         println("Game Event List ${GameEventConsumerService.gameEventList}")
-        assertThat(GameEventConsumerService.gameEventList.size != 0).isTrue
+        latch.await(3L,TimeUnit.SECONDS)
+        assertEquals(1,GameEventConsumerService.gameEventList.size)
     }
 }
