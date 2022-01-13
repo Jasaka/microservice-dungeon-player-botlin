105da14e33fbc9b4a1e5539ca4165bb22dbe531c
andma <andreas.ma@smail.th-koeln.de>
2022-01-13 21:51:48 +0100

    fix: some classes were using class Player but were supposed to use class User fix: more complete User service

--- src/main/kotlin/thkoeln/dungeon/botlin/user/User.kt
+++ src/main/kotlin/thkoeln/dungeon/botlin/user/User.kt
@@ -1,26 +1,40 @@
 package thkoeln.dungeon.botlin.user
 
 import thkoeln.dungeon.botlin.game.domain.Game
+import thkoeln.dungeon.botlin.player.Player
 import java.util.*
 import javax.persistence.*
+import kotlin.jvm.Throws
 
 @Entity
-class User {
+class User() {
     @Id
     var id = UUID.randomUUID()
 
     //TODO bind to front end
-    lateinit var name : String
-    lateinit var email : String
-    lateinit var bearerToken : UUID
+    lateinit var name: String
+    lateinit var email: String
+    lateinit var bearerToken: UUID
 
-    @OneToMany(cascade = [CascadeType.MERGE,CascadeType.REMOVE], fetch = FetchType.EAGER)
-    var games : List<Game> = arrayListOf()
-    fun isReadyToPlay() : Boolean = bearerToken != null
+    @OneToMany(cascade = [CascadeType.MERGE, CascadeType.REMOVE], fetch = FetchType.EAGER)
+    var players: MutableList<Player> = arrayListOf()
+    fun isReadyToPlay(): Boolean = bearerToken != null
 
-    fun participateInGame(game : Game)
-    {
-        game.
-
+    fun participate() : Player {
+        var player = Player()
+        players.add(player)
+        return player
     }
+
+    fun findPlayerInGame(game: Game): Boolean {
+        for (player in players) {
+            for (playerGame in game.players) {
+                return playerGame == player
+            }
+        }
+        return false;
+    }
+
+    @Override
+    override fun toString(): String = "Player '$name' (bearerToke: $bearerToken)"
 }
--- src/main/kotlin/thkoeln/dungeon/botlin/user/UserService.kt
+++ src/main/kotlin/thkoeln/dungeon/botlin/user/UserService.kt
@@ -1,4 +1,14 @@
 package thkoeln.dungeon.botlin.user
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
 class UserService {
+    lateinit var userRepository: UserRepository
+
+    @Autowired
+    constructor(userRepository: UserRepository) {
+        this.userRepository = userRepository
+    }
 }
