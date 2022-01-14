2e1d488c76e4198cb7c67048871a0c62a8e8a8cf
Jan Samak <accounts@jan-samak.de>
2022-01-14 13:21:01 +0100

    feat: controller and adapter endpoints and wirering

--- src/main/kotlin/thkoeln/dungeon/botlin/api/GameAdapter.kt
+++ src/main/kotlin/thkoeln/dungeon/botlin/api/GameAdapter.kt
@@ -1,46 +1,83 @@
 package thkoeln.dungeon.botlin.api
 
-import khttp.responses.Response
+import org.json.JSONObject
+import org.springframework.http.*
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
 import java.util.*
 
-class GameAdapter {
 
-    private val gameURL: String = "http://localhost:8081"
+@Service
+class GameAdapter() {
 
-    fun createPlayer() {
-        val request = khttp.post(gameURL)
-        println(request)
+    private val restTemplate = RestTemplate()
+    private val gameBaseURL: String = "http://localhost:8080"
+
+    fun createPlayer(name: String, email: String): String? {
+        val jsonObject = JSONObject()
+        jsonObject.put("name", name)
+        jsonObject.put("email", email)
+        return postObject(gameBaseURL)
     }
 
-    fun registerPlayer(gameID: UUID, playerId: UUID) {
-        val request = khttp.put("$gameURL/games/$gameID/players/$playerId")
-        println(request)
+    fun registerPlayer(gameId: UUID, playerId: UUID): String? {
+        return putValue("$gameBaseURL/games/$gameId/players/$playerId")
     }
 
-    fun getCurrentTime(gameID: UUID) {
-        val request = khttp.get("$gameURL/games/$gameID/time")
-        println(request)
+    fun getCurrentTime(gameId: UUID): String? {
+        return getJSON("$gameBaseURL/games/$gameId/time")
     }
 
-    fun getRunningGames() {
-        val request = khttp.get("$gameURL/games")
-        println(request)
+    fun getAllGames(): String? {
+        return getJSON("$gameBaseURL/games")
     }
 
-    fun getGame(gameId: UUID): Response {
-        val request = khttp.get("$gameURL/games/$gameId")
-        println(request)
-        return request
+    fun getGame(gameId: UUID): String? {
+        return getJSON("$gameBaseURL/games/$gameId")
     }
 
-    fun getCommandsOfRound(gameID: UUID) {
-        val request = khttp.get("$gameURL/games/$gameID")
-        println(request)
+    fun getCommandsOfRound(gameId: UUID): String? {
+        return getJSON("$gameBaseURL/games/$gameId")
     }
 
-    fun issueCommand(gameID: UUID) {
-        val request = khttp.post("$gameURL/games/$gameID")
-        println(request)
+    fun issueCommand(gameId: UUID): String? {
+        val jsonObject = JSONObject()
+        jsonObject.put("gameId", gameId)
+        return postObject("$gameBaseURL/commands")
+    }
+
+    fun getJSON(targetUrl: String): String? {
+        return restTemplate.getForObject(targetUrl, String::class.java)
+    }
+
+    fun postObject(url: String, jsonObject: JSONObject = JSONObject()): String? {
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_JSON
+
+        val request: HttpEntity<String> = HttpEntity(jsonObject.toString(), headers)
+
+        val response = restTemplate.exchange(url, HttpMethod.POST, request, String::class.java)
+
+        return if (response.statusCode == HttpStatus.OK) {
+            response.body
+        } else {
+            null
+        }
+    }
+
+    fun putValue(url: String, jsonObject: JSONObject = JSONObject()): String? {
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_JSON
+
+        val request: HttpEntity<String> = HttpEntity(jsonObject.toString(), headers)
+
+        val response = restTemplate.exchange(url, HttpMethod.PUT, request, String::class.java)
+
+        return if (response.statusCode == HttpStatus.OK) {
+            response.body
+        } else {
+            null
+        }
     }
 
 }
