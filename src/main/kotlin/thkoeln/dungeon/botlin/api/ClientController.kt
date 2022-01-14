90907b30c9a8dd294d4e3ed522fb64e71be88e61
Jan Samak <accounts@jan-samak.de>
2022-01-14 13:21:01 +0100

    feat: controller and adapter endpoints and wirering

--- src/main/kotlin/thkoeln/dungeon/botlin/api/ClientController.kt
+++ src/main/kotlin/thkoeln/dungeon/botlin/api/ClientController.kt
@@ -1,5 +1,4 @@
 package thkoeln.dungeon.botlin.api
-import khttp.responses.Response
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -23,49 +22,63 @@ class ClientController {
     }
 
     @GetMapping("/games")
-    fun getGames(): String = "All Games"
+    fun getGames(): String? = adapter.getAllGames()
 
     @GetMapping("/games/{game-id}")
-    fun getGame(@PathVariable("game-id") gameId: UUID): Response {
-        println(gameId)
-        return adapter.getGame(gameId)
-    }
+    fun getGame(@PathVariable("game-id") gameId: UUID): String? = adapter.getGame(gameId)
 
+    @GetMapping("/games/{game-id}/time")
+    fun getGameTime(@PathVariable("game-id") gameId: UUID): String? = adapter.getCurrentTime(gameId)
+
+    @GetMapping("/games/{game-id}/commands")
+    fun getCommandsOfRound(@PathVariable("game-id") gameId: UUID): String? = adapter.getCommandsOfRound(gameId)
+
+    // TODO: Mapping
     @GetMapping("/games/{game-id}/robots")
-    fun getRobots(@PathVariable("game-id") gameId: UUID): String = "All Robots in Game $gameId"
+    fun getRobots(@PathVariable("game-id") gameId: UUID): String? = "All Robots in Game $gameId"
 
+    // TODO: Mapping
     @GetMapping("/games/{game-id}/robots/{robot-id}")
-    fun getRobot(@PathVariable("game-id") gameId: UUID, @PathVariable("robot-id") robotId: UUID): String = "Robot $robotId in Game $gameId"
+    fun getRobot(@PathVariable("game-id") gameId: UUID, @PathVariable("robot-id") robotId: UUID): String? = "Robot $robotId in Game $gameId"
 
+    // TODO: Mapping
     @GetMapping("/games/{game-id}/robots/{robot-id}/energy")
-    fun getRobotEnergy(@PathVariable("game-id") gameId: UUID, @PathVariable("robot-id") robotId: UUID): String = "Energy of Robot $robotId in Game $gameId"
+    fun getRobotEnergy(@PathVariable("game-id") gameId: UUID, @PathVariable("robot-id") robotId: UUID): String? = "Energy of Robot $robotId in Game $gameId"
 
+    // TODO: Mapping
     @GetMapping("/games/{game-id}/robots/{robot-id}/items")
-    fun getRobotItems(@PathVariable("game-id") gameId: UUID, @PathVariable("robot-id") robotId: UUID): String = "Items of Robot $robotId in Game $gameId"
+    fun getRobotItems(@PathVariable("game-id") gameId: UUID, @PathVariable("robot-id") robotId: UUID): String? = "Items of Robot $robotId in Game $gameId"
 
+    // TODO: Mapping
     @GetMapping("/games/{game-id}/robots/{robot-id}/ressources")
-    fun getRobotRessources(@PathVariable("game-id") gameId: UUID, @PathVariable("robot-id") robotId: UUID): String = "Ressources of Robot $robotId in Game $gameId"
+    fun getRobotRessources(@PathVariable("game-id") gameId: UUID, @PathVariable("robot-id") robotId: UUID): String? = "Ressources of Robot $robotId in Game $gameId"
 
+    // TODO: Mapping
     @GetMapping("/games/{game-id}/map/planets")
-    fun getKnownPlanets(@PathVariable("game-id") gameId: UUID): String = "Planets in Game $gameId"
+    fun getKnownPlanets(@PathVariable("game-id") gameId: UUID): String? = "Planets in Game $gameId"
 
+    // TODO: Mapping
     @GetMapping("/games/{game-id}/map/planets/{planet-id}")
-    fun getPlanet(@PathVariable("game-id") gameId: UUID, @PathVariable("planet-id") planetId: UUID): String = "Planet $planetId in Game $gameId"
+    fun getPlanet(@PathVariable("game-id") gameId: UUID, @PathVariable("planet-id") planetId: UUID): String? = "Planet $planetId in Game $gameId"
 
+    // TODO: Mapping
     @GetMapping("/games/{game-id}/map")
-    fun getMap(@PathVariable("game-id") gameId: UUID): String = "Map of Game $gameId"
+    fun getMap(@PathVariable("game-id") gameId: UUID): String? = "Map of Game $gameId"
 
+    // TODO: Mapping
     @GetMapping("/games/{game-id}/player")
-    fun getPlayer(@PathVariable("game-id") gameId: UUID): String = "Player status in Game $gameId"
+    fun getPlayer(@PathVariable("game-id") gameId: UUID): String? = "Player status in Game $gameId"
 
+    // TODO: Mapping
     @PostMapping("/games/{game-id}/robots/{robot-id}/command")
     fun issueCommand(@PathVariable("game-id") gameId: UUID, @PathVariable("robot-id") robotId: UUID): String = "Command to $robotId"
 
+    // TODO: Mapping
     @PutMapping("/games/{game-id}/robots/{robot-id}/mode")
     fun changeRobotMode(@PathVariable("game-id") gameId: UUID, @PathVariable("robot-id") robotId: UUID): String = "Robot $robotId Mode"
 
+    // TODO: Mapping
     @PutMapping("/games/{game-id}/robots/mode")
     fun changeSwarmMode(@PathVariable("game-id") gameId: UUID): String = "Swarm Mode"
-
 
 }
