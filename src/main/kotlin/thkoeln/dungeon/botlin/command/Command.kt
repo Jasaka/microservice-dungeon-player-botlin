03957335d552dee2aa40dd9ea1fafa18f3974163
jblekic <jasmin.blekic@smail.th-koeln.de>
2022-01-16 17:46:14 +0100

    feat: added Command

---
+++ src/main/kotlin/thkoeln/dungeon/botlin/command/Command.kt
@@ -0,0 +1,18 @@
+package thkoeln.dungeon.botlin.command
+
+import java.util.*
+
+class Command {
+
+    private lateinit var gameId : UUID
+
+    private lateinit var playerId: UUID
+
+    private lateinit var robotId: UUID
+
+    private lateinit var commandType: CommandType
+
+    fun sendAllCommands(){
+
+    }
+}
