47272e3ba61a9eaf9e0c48210426d0839b962761
jblekic <jasmin.blekic@smail.th-koeln.de>
2022-01-16 17:47:49 +0100

    feat: added CommandObject

---
+++ src/main/kotlin/thkoeln/dungeon/botlin/command/CommandObject.kt
@@ -0,0 +1,25 @@
+package thkoeln.dungeon.botlin.command
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import lombok.Getter
+import lombok.NoArgsConstructor
+import lombok.Setter
+import lombok.ToString
+import java.util.*
+
+@Setter
+@Getter
+@NoArgsConstructor
+@ToString
+
+class CommandObject {
+    private lateinit var commandType: CommandType
+
+    private lateinit var planetId : UUID
+
+    private lateinit var targetId  : UUID
+
+    private lateinit var itemName: String
+
+    private var itemQuantity: Int = -1
+}
